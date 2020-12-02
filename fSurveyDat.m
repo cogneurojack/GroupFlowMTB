c277ba4fbfd747eb4b5d4d50eabdef9b8254ae0b
Jack Moore <moorejack@rocketmail.com>
2020-12-02 16:53:21 +0000

    Cleaning
    can compare the dates the data row was compared in the two data file now...still need to be implemented within the main code.

--- fSurveyDat.m
+++ fSurveyDat.m
@@ -13,18 +13,16 @@ end
 
 %% TESTING NEW METHODS TO TRY AND ENSURE WE GET DATE INFO
 
-testData = readtable('/Users/jackmoore/Desktop/allSurveyClean.csv');
+surveyData = readtable('/Users/jackmoore/Desktop/allSurveyClean.csv');
 
- testData(15,1); % the datetime of the sruvey data (14 is the last rwo it works correctly for :\
- testData = datevec(testData{15,1});
- testData = [num2str(x(3)) '/' num2str(x(2)) '/' num2str(x(1))]
- 
- fileDat = dataFilesInfo(1).date; % the datetime of the performance data file
- 
-myDateNum = datenum( fileDat);
- 
-y = datevec(myDateNum, 'DD/MM/YYYY')
+% will have to look call this within the function
+sDate = datevec(surveyData{iSurvey,1});
+sDate = [num2str(sDate(3)) '/' num2str(sDate(2)) '/' num2str(sDate(1))];
 
-y = [num2str(x(3)) '/' num2str(x(2)) '/' num2str(x(1))]
+% can call this for each subj pair
+fileDate = dataFilesInfo(1).date; % the datetime of the performance data file
+fDate = datenum(fileDate);
+fDate = datevec(fDate, 'DD/MM/YYYY');
+fDate = [num2str(x(3)) '/' num2str(x(2)) '/' num2str(x(1))];
 
- testData == y
+sDate == fDate
