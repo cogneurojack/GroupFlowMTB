2bd601cb20ad269efd3eb697344a7e480ca87d7a
Jack Moore <moorejack@rocketmail.com>
2020-12-02 16:34:00 +0000

    Check dates works
    worked out how to convert the date info from both performance and survey data into the correct format so theay can be compared.

--- fSurveyDat.m
+++ fSurveyDat.m
@@ -11,8 +11,20 @@ for iSurvey = 2:length(surveyData)
     end
 end
 
-%% TESTIG NEW METHODS
+%% TESTING NEW METHODS TO TRY AND ENSURE WE GET DATE INFO
 
 testData = readtable('/Users/jackmoore/Desktop/allSurveyClean.csv');
 
- testData{14,1}
+ testData(15,1); % the datetime of the sruvey data (14 is the last rwo it works correctly for :\
+ testData = datevec(testData{15,1});
+ testData = [num2str(x(3)) '/' num2str(x(2)) '/' num2str(x(1))]
+ 
+ fileDat = dataFilesInfo(1).date; % the datetime of the performance data file
+ 
+myDateNum = datenum( fileDat);
+ 
+y = datevec(myDateNum, 'DD/MM/YYYY')
+
+y = [num2str(x(3)) '/' num2str(x(2)) '/' num2str(x(1))]
+
+ testData == y
