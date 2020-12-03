ef096c468c904e687306057df623c596189648a9
Jack Moore <moorejack@rocketmail.com>
2020-12-03 14:17:22 +0000

    date  length issue sorted
    sorted the date langth issue, but now have missing trial data.

--- fSurveyDat.m
+++ fSurveyDat.m
@@ -10,10 +10,12 @@ trialName = trialName{2}(1:end-4);
 for iSrvy = 1:size(sData,1)
     
     % format survey data datestamp to be same as file data datestamp
-    sDate = datevec(sData{iSrvy,1});
+    sDate = datenum(sData{iSrvy,1});
+    sDate = datevec(sDate);
     sDate = [num2str(sDate(3)) '/' num2str(sDate(2)) '/' num2str(sDate(1))];
     %% CHECK IF SAME AS TEST DATA
-    if length(sData.Activity{iSrvy})== length(trialName)
+    if length(sData.Activity{iSrvy})== length(trialName)...
+        && length(sDate) == length(fDate)
         if all(biosemi{1} ==  sData.ParticipantName{iSrvy}(1:2)) == true...  % same subjID
                 && all(sData.Activity{iSrvy}==trialName)==true...             % same trial
                 && all(sDate == fDate)==true                            % same day
