0c2a1fd1cb9010f344adb5749964305a6c779a95
Jack Moore <moorejack@rocketmail.com>
2020-12-03 13:07:50 +0000

    survey trial by trial
    Trying to goe through each trial and add to the matrix

--- fSurveyDat.m
+++ fSurveyDat.m
@@ -1,8 +1,10 @@
-function  [subjSData] = fSurveyDat(biosemi,sData,fDate,trialNum)
+function  [trialSurveyDat] = fSurveyDat(fileName,sData,fDate)
 %% FUNCTION SET-UP
 % create output cellarray
-t = 1;
-subjSData = sData(t,:);
+biosemi = {fileName(1:2) fileName(3:4)};
+trialName = regexp(fileName,'_','split');
+trialName = trialName{2}(1:end-4);
+
 %% LOOP THROUGH SURVEY DATA FILE
 for iSrvy = 2:size(sData,1)
     
@@ -10,12 +12,13 @@ for iSrvy = 2:size(sData,1)
     sDate = datevec(sData{iSrvy,1});
     sDate = [num2str(sDate(3)) '/' num2str(sDate(2)) '/' num2str(sDate(1))];
     %% CHECK IF SAME AS TEST DATA
-    
-    if all(biosemi{1} ==  sData.ParticipantName{iSrvy}) == true...  % same subjID
-            && str2num(sData.Trial{iSrvy})==trialNum...             % same trial
-            && all(sDate == fDate)==true                            % same day
-        subjSData(t,:) = sData(iSrvy,:);
-        t=t+1;
+    if length(sData.Activity{iSrvy})== trialName
+        if all(biosemi{1} ==  sData.ParticipantName{iSrvy}(1:2)) == true...  % same subjID
+                && all(sData.Activity{iSrvy}==trialName)==true...             % same trial
+                && all(sDate == fDate)==true                            % same day
+            trialSurveyDat = sData(iSrvy,:);
+            t=t+1;
+        end
     end
 end
 end
